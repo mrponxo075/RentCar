8244a108946305ff68d292c3712bca9412b951eb
mrponxo075 <foursteelstrings@hotmail.com>
2025-11-25 13:37:54 +0100

    Add test xUnit proyect. Add new class for testing tax methods service.

---
+++ RentCart.Application.Test/Services/TaxServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using Moq;
+using RentCar.Application.Contracts.Repositories;
+using RentCar.Application.Dtos.Requests;
+using RentCar.Application.Mappings;
+using RentCar.Application.Services;
+using RentCar.Application.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentCart.Application.Test.Services
+{
+    public class TaxServiceTests
+    {
+        private readonly Mock<ITaxRepository> _taxRepositoryMock;
+        private readonly Mock<ILogger<TaxService>> _loggerMock;
+        private readonly TaxService _taxService;
+
+        public TaxServiceTests()
+        {
+            _taxRepositoryMock = new Mock<ITaxRepository>();
+            _loggerMock = new Mock<ILogger<TaxService>>();
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<AutoMapperProfile>();
+            },
+            null);
+
+            _taxService = new TaxService(
+                _taxRepositoryMock.Object,
+                new TaxDtoValidator(),
+                config.CreateMapper(),
+                _loggerMock.Object
+            );
+        }
+    }
+}
